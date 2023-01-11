4006c05a0f12b28cb2a661ac45eb2403e4b6bf08
luiscg01 <luiscg0101@gmail.com>
2023-01-11 11:52:46 +0100

    Columnas

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,11 +5,21 @@ class HomeScreen extends StatelessWidget {
 
  
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.red,
-      body:  Center(
-          child: Text('HOME SCREEN'),
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('HOME SCREEN'),
+        elevation: 10.0,
+      ),
+      body: Center(
+        child: Column (
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const <Widget>[
+                Text('CONTADOR DE CLICKS'),
+                Text('10'),
+              ],
+            ),
+      ),
     );
   }
 
