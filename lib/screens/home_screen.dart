89aae80f79eeab425186aea020da7add58fad4d7
luiscg01 <luiscg0101@gmail.com>
2023-01-11 12:06:35 +0100

    Boton Flotante

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -20,6 +20,12 @@ class HomeScreen extends StatelessWidget {
               ],
             ),
       ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          print('HOLA MUNDO');
+        } ,
+      ),
     );
   }
 
