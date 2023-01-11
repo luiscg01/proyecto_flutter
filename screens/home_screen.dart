e7f98552f2d60add14848da6204132b49ab7a875
luiscg01 <120457538+luiscg01@users.noreply.github.com>
2023-01-11 11:12:07 +0100

    Add files via upload

---
+++ screens/home_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+ 
+  Widget build(BuildContext context) {
+    return const Center(
+        child: Text('HOME_SCREEN'),
+      );
+  }
+
+}
