cd668a43f227950dfee01133430ef49271a4648f
luiscg01 <luiscg0101@gmail.com>
2023-01-11 11:05:34 +0100

    Implementacion de archivos

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,17 @@
+import 'package:counter_app/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Center(
-        child: Text('HOLA MUNDO'),
-      )
+    return const MaterialApp(
+      home: HomeScreen()
     );
   }
 
